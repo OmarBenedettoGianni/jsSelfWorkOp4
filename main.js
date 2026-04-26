5daafae28a0d5a783df5893c3a85f57f1b87aaf9
OmarBenedettoGianni <omarbenedetto200012@gmail.com>
2026-04-26 17:29:16 +0200

    calcolate file gatti mancanti e gatti avanzo

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 let numGatti =parseInt(prompt(`Inserisci il numero dei Gatti`));
 let numGattiFile =parseInt(prompt(`Inserisci il numero dei Gatti in fila`));
+let numeroFile = Math.floor(gattiTotali / gattiInFila);
+let gattiAvanzo = gattiTotali % gattiInFila;
+let gattiMancanti = gattiInFila - gattiAvanzo;
+console.log(`Ci sono ${numeroFile} file di gatti e ne mancano ${gattiMancanti} per una nuova fila, con un avanzo di ${gattiAvanzo}`);
